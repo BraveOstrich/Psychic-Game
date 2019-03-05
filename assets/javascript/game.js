ffc4fcb59da0e2ecd3dc276f77fe03451746bcff
Steve White <stevewhitecodes@gmail.com>
2019-03-05 15:25:36 -0600

    Game is functional, ready for basic css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Random letter generator
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    var options = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 // Count wins & losses
     var wins = 0;
     var losses = 0;
@@ -11,8 +11,8 @@
     var lettersGuessed = [];
 
 // Variables to html document
-    var winsText = document.getElementById("wins-text").innerHTML;
-    var lossesText = document.getElementById("losses-text").innerHTML;
+    var winsText = document.getElementById("wins-text");
+    var lossesText = document.getElementById("losses-text");
     var guessesLeft = document.getElementById("guesses-left");
     var guessedLetters = document.getElementById("your-guesses");
 
@@ -24,23 +24,35 @@
 // Function ran when user selects a letter
     document.onkeyup = function(event) {
 // Determines which letter was pressed
-        var userGuess = event.key;
+    var userGuess = event.key;
 
+    
+    // for (var i = 1; i < guessLeft; i++){
+    //     //guessedLetters = [];
+    //     console.log(computerChoice);
+    // }
     if (userGuess === computerChoice){
-        wins ++;
-        guessedLetters = [];
+        wins++;
+        lettersGuessed = [];
+    } else {
+        guessLeft--;
+
+        if(guessLeft <= 0){
+            losses++;
+            guessLeft = 9;
+            lettersGuessed = [];
+            computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+        }
+        else{
+            lettersGuessed.push(userGuess);
+        }
+        
     } 
-    if (userGuess !== computerChoice){
-        guessLeft --;
-    }
-    if (guessLeft === 0){
-        losses ++;
-        guessedLetters = [];
-    }
 
 
-    guessedLetters.textContent += " " + userGuess + ",";
-    winsText.textContent += wins;
-    lossesText.textContent += lossesText;
-    guessesLeft.textContent += guessLeft;
+    guessedLetters.textContent = "Your guesses so far: " + lettersGuessed;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesLeft.textContent = "Guesses left: " + guessLeft;
+
 };
